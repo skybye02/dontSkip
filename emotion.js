805f2a4c18cb3d0ab9668eab0b37b7dc2c90bb7e
skybye02 <71697644+skybye02@users.noreply.github.com>
2020-09-25 08:10:05 +0530

    Update emotion.js

--- emotion.js
+++ emotion.js
@@ -1,1 +1,16 @@
 # dontskip
+
+toxic relation has many form 
+
+when u r in  relation then suddenly u see bye effect 
+
+then never cry never think omg 
+
+why this has happened 
+
+if love is there then no bye no toxic pain rejection injection 
+
+understand this thing always 
+
+so to think about poison means u r still consuming poison 
+
