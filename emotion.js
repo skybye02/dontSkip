025748ae0bfbb527a24463472303915a43b7ee1f
skybye02 <71697644+skybye02@users.noreply.github.com>
2020-09-25 08:13:08 +0530

    Update emotion.js

--- emotion.js
+++ emotion.js
@@ -14,3 +14,19 @@ understand this thing always
 
 so to think about poison means u r still consuming poison 
 
+
+
+so never look back on track which was like toxic 
+
+
+this note especially for teenagers and innocent all innocent soul 
+
+thats it its a final note 
+
+that why not to think on toxic injection effect 
+
+its common some pain will be there bcz of the environment which u were thinking like its heaven 
+
+but undesrstand that environment was illusion 
+
+nothing else so never think and push ur brain heart in pain 
